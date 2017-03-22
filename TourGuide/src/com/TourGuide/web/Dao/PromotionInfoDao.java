ac8cb9c272bae8b3c4bc3752eb482b21457fb62d
wxt19930825 <494187797@qq.com>
2017-03-22 22:24:56 +0800

    aa
    Signed-off-by: wxt19930825 <494187797@qq.com>

--- TourGuide/src/com/TourGuide/web/Dao/PromotionInfoDao.java
+++ TourGuide/src/com/TourGuide/web/Dao/PromotionInfoDao.java
@@ -1,8 +1,15 @@
 package com.TourGuide.web.Dao;
 
+import java.sql.Connection;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.TourGuide.web.model.PromotionInfo;
 
 
 @Repository
@@ -10,6 +17,19 @@ public class PromotionInfoDao {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
+	
+	
+	/**
+	 * 分页得到所有的景区活动信息
+	 * @return 所有景区的活动信息
+	 * 2017-3-22 21:37:17
+	 */
+	public List<PromotionInfo> getPromotionInfoBypage(int i,int j) {
+		
+		DataSource dataSource=jdbcTemplate.getDataSource();
+		
+	}
+	
 	
 	
 	
